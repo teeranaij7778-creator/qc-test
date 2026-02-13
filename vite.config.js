1f9b14a24cd34bebe0ced9b7e16e9da1f4bc6da2
jji-102 <51739271+jji-102@users.noreply.github.com>
2026-02-13 10:13:50 +0700

    Update: Fix PostCSS config and add deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/qc-test/', // ใส่ชื่อ Repository ของเพื่อนตรงนี้
   plugins: [
     react(),
     tailwindcss(),
