d4547c58ce71ab6181c8ff9c1e7dd00a63cdf505
jji-102 <51739271+jji-102@users.noreply.github.com>
2026-02-13 10:17:23 +0700

    Update vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/qc-test/', // ใส่ชื่อ Repository ของเพื่อนตรงนี้
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/qc-test/', 
+  plugins: [react(), tailwindcss()],
 })
